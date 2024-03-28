0bf1f6173e82d04910ae1ea71f7a2ec1ebc0bb45
Jacob DiBiase <jakesback93@hotmail.com>
2024-03-28 00:10:43 -0700

    Create an array for Pokemon data to display

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-let favoriteFood = 'Bulgogi';
+//contains Pokemon data to display from an array of objects
+let pokemonList = [
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+    {name: 'Charizard', height: 17, types: ['fire', 'flying']},
+    {name: 'Pikachu', height: 4, types: 'electric'}
+];
 
-alert('Hello world');
-
-document.write(favoriteFood);
