51fc130f063c0aca1d5879d34d486ed29e748415
Jacob DiBiase <jakesback93@hotmail.com>
2024-03-28 00:18:02 -0700

    Add abilities to the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //contains Pokemon data to display from an array of objects
 let pokemonList = [
-    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-    {name: 'Charizard', height: 17, types: ['fire', 'flying']},
-    {name: 'Pikachu', height: 4, types: 'electric'}
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison'], abilities: ['chlorophyll', 'overgrow']},
+    {name: 'Charizard', height: 17, types: ['fire', 'flying'], abilities: ['blaze', 'solar-power']},
+    {name: 'Pikachu', height: 4, types: 'electric', abilities: ['static', 'lightningrod']}
 ];
 
