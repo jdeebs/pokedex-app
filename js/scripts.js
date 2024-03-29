2cbd89bbd07b871693bdd35bb8806f5da0171d40
Jacob DiBiase <jakesback93@hotmail.com>
2024-03-29 13:03:39 -0700

    Add basis for CSS styling including colors and variables

--- js/scripts.js
+++ js/scripts.js
@@ -29,14 +29,15 @@ let pokemonList = [
 // Loop through each Pokemon in the pokemonList array
 for (let i = 0; i < pokemonList.length; i++) {
   // Write a message with the current Pokemon name and height
-  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}m)`);
+  document.write(`${pokemonList[i].name} (height: 
+    ${pokemonList[i].height}m)`);
   // Check if the height of the current Pokemon is between 1-2 meters
   if (pokemonList[i].height > 1 && pokemonList[i].height < 2) {
     // If height of current Pokemon is between 1-2 meters, write a message indicating the Pokemon is taller than 1 meter
-    document.write(pokemonList[i].name + " is taller than 1 meter!");
+    document.write(` - ${pokemonList[i].name} is taller than 1 meter!`);
   } else if (pokemonList[i].height < 1.0) {
     // If height of current Pokemon is less than 1 meter, write a message indicating the Pokemon is shorter than 1 meter
-    document.write(pokemonList[i].name + " is shorter than 1 meter!");
+    document.write(` - ${pokemonList[i].name} is shorter than 1 meter!`);
   } else {
     // If height of current Pokemon is taller than 2 meters, write a message indicating it's big
     document.write(" Wow, that's big!");
