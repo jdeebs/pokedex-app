d24f4a2c0848dadc5da82fa2a3178586b4905ae8
Jacob DiBiase <jakesback93@hotmail.com>
2024-04-01 13:05:23 -0700

    Update loop to display Pokemon details using getAll function

--- js/scripts.js
+++ js/scripts.js
@@ -41,12 +41,15 @@ let pokemonRepository = (function () {
   // Return an object with 2 public functions assigned as keys
   return {
     add: add, // Expose the add function
-    getAll: getAll // Expose the getAll function
+    getAll: getAll, // Expose the getAll function
   };
 })();
 
+// Get the Pokemon list using getAll() function
+let pokemonDetails = pokemonRepository.getAll();
+
 // Call pokemonList loop function
-pokemonList.forEach(pokemonListLoop);
+pokemonDetails.forEach(pokemonListLoop);
 
 // Create Pokedex container wrapper for styling
 document.write(`<div class="pokedex-container-wrapper">`);
@@ -55,30 +58,30 @@ document.write(`<div class="pokedex-container-wrapper">`);
 document.write(`<div class="pokedex-container">`);
 
 // Loop through each Pokemon in the pokemonList array
-function pokemonListLoop(pokemonList) {
+function pokemonListLoop(pokemonDetails) {
   // Start a new div section for each Pokemon
   document.write(`<div class="pokemon">`);
 
   // Write a message with the current Pokemon name and height
   document.write(`<div class="pokemon-info">`);
   document.write(
-    `${pokemonList.name} (height: ${pokemonList.height}m)
+    `${pokemonDetails.name} (height: ${pokemonDetails.height}m)
     `
   );
   document.write(`</div>`);
 
   // Check if the height of the current Pokemon is between 1-2 meters
   document.write(`<div class="height-info">`);
-  if (pokemonList.height > 1 && pokemonList.height < 2) {
+  if (pokemonDetails.height > 1 && pokemonDetails.height < 2) {
     // If height of current Pokemon is between 1-2 meters, write a message indicating the Pokemon is taller than 1 meter
     document.write(
-      `${pokemonList.name} is taller than 1 meter!
+      `${pokemonDetails.name} is taller than 1 meter!
       `
     );
-  } else if (pokemonList.height < 1.0) {
+  } else if (pokemonDetails.height < 1.0) {
     // If height of current Pokemon is less than 1 meter, write a message indicating the Pokemon is shorter than 1 meter
     document.write(
-      `${pokemonList.name} is shorter than 1 meter!
+      `${pokemonDetails.name} is shorter than 1 meter!
       `
     );
   } else {
